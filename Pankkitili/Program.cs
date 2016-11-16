689aa78c5cccd8123f6a415bef7b5d5007004f75
ao136524 <ao136524@NKP-WS3754>
2016-11-16 10:52:24 +0200

    Add project files.

---
+++ Pankkitili/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pankkitili
+{
+    class Pankkitili
+    {
+        string omistaja;
+        string tili;
+        int saldo;
+
+        public void nayta_tilin_tiedot()
+        {
+            Console.WriteLine("Tilin "+tili+", "+omistaja+" tiedot");
+            Console.WriteLine("Saldo: " + saldo + "e");
+            Console.WriteLine("----------------------------------");
+        }
+
+
+        public void talleta_rahaa()
+        {
+
+        }
+
+        public void nosta_rahaa()
+        {
+
+        }
+
+        public void siirra_rahaa_tilille()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Pankkitili formula;
+
+            formula = new Pankkitili();
+
+            formula.tili = ("CH12 5476 6699 1536 35");
+            formula.omistaja = ("Rimi Käikkönen");
+            formula.saldo = 45888;
+
+            formula.nayta_tilin_tiedot();
+
+            Pankkitili koodari;
+
+            koodari = new Pankkitili();
+
+            koodari.omistaja = ("Jördekurren");
+            koodari.tili = ("SV55 4854 4813 1552 14");
+            koodari.saldo = 198111;
+
+            koodari.nayta_tilin_tiedot();
+
+            Console.ReadKey();
+        }
+    }
+}
